b9580f97eebdb766c4c8c3c527d8f04b187cdbcd
Fernando Contreras <fercreek@gmail.com>
2016-12-08 04:26:00 -0600

    Accordion with sanitize

--- js/logic.js
+++ js/logic.js
@@ -1,13 +1,21 @@
-angular.module('idcyta', [])
+angular.module('idcyta', ['ui.bootstrap', 'ngSanitize'])
 	.controller('ArticleController', ArticleController)
 	.factory('ArticleService', ArticleService);
 
-function ArticleController(ArticleService) {
+function ArticleController($sce, ArticleService) {
 	var vm = this;
-	vm.chapters = ArticleService.chapters;
 	vm.valueEnable = 0;
+	vm.oneAtATime = true;
 
+	vm.chapters = ArticleService.chapters;
+	vm.subChapters = ArticleService.subChapters;
 	vm.openVol = openVol;
+
+	vm.status = {
+	  isCustomHeaderOpen: false,
+	  isFirstOpen: true,
+	  isFirstDisabled: false
+	};
 
 	function openVol(val) {
 		vm.valueEnable = val;
@@ -22,7 +30,19 @@ function ArticleService() {
 		'Evaluación sensorial', 'Cárnicos', 'Desarrollo de nuevos productos', 'Alimentos funcionales', 'Otros'
 	];
 
+	var subChapters = [
+		{	
+			title: 'CARACTERIZACIÓN DE LAS PROTEÍNAS DEL FRIJOL AYOCOTE (<em>Phaseolus coccineus L.</em>)', 
+			authors: 'Teniente-Martínez G., González Cruz L., Cariño-Cortés R., Bernardino-Nicanor A'
+		}, 
+		{
+			title: 'CARACTERIZACIÓN DE POBLACIONES DE MEDIOS HERMANOS DE MAÍZ, DE LA REGIÓN DEL BAJÍO PARA SU USO EN LA INDUSTRIA TORTILLERA',
+			authors: 'Gutiérrez Tlahque Jorge, Sebastian Mar Luz Arely , Ramírez Pimentel Juan Gabriel, Raya Pérez Juan Carlos'
+		}
+	];
+
 	return {
-		chapters : chapters
+		chapters : chapters,
+		subChapters: subChapters
 	}
 }
